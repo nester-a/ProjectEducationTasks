f7659adcf9e7c8d60d440a6bfd5fa63b07aab956
Konstantine <kach91kach91@gmail.com>
2022-10-30 17:24:04 +0300

    Update IPlayer.cs

--- 03_PlayersDB/Interfaces/IPlayer.cs
+++ 03_PlayersDB/Interfaces/IPlayer.cs
@@ -1,6 +1,6 @@
 ﻿namespace _03_PlayersDB.Interfaces
 {
-    internal interface IPlayer
+    internal interface IPlayer//Это очень странно. Часть свойств можно задачать через set - часть нет, логики никакой я в этом не вижу.
     {
         int Id { get; set; }//Пустые строки меж методами
         string Name { get; }
